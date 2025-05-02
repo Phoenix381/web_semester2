9d81c279feb708e07af69337d473dda5e64a2f3b
phoenix381 <phoenix0381@gmail.com>
2025-05-02 10:53:40 +0300

    adding reviews

--- content/src/Controller/ProductController.php
+++ content/src/Controller/ProductController.php
@@ -8,24 +8,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Review;
+use App\Form\ReviewTypeForm;
+
 class ProductController extends AbstractController
 {
     #[Route('/product/{id}', name: 'product_show')]
-    public function show(Product $product, ProductRepository $productRepository): Response
+    public function show(Product $product, Request $request, ProductRepository $productRepository, EntityManagerInterface $em): Response
     {
-        // The Product $product parameter will be automatically fetched by Doctrine
-        // thanks to Symfony's ParamConverter when the {id} is provided in the URL
+        $review = new Review();
+        $form = $this->createForm(ReviewTypeForm::class, $review);
         
-        // If you need more control, you can fetch it manually:
-        // $product = $productRepository->find($id);
-        
-        // Or handle not found case:
-        if (!$product) {
-            throw $this->createNotFoundException('Product not found');
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $review->setProduct($product);
+            // $review->setCreatedAt(new \DateTimeImmutable());
+            
+            // If you have user authentication
+            if ($this->getUser()) {
+                $review->setOwner($this->getUser());
+            }
+            
+            $em->persist($review);
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', ['id' => $product->getId()]);
         }
 
         return $this->render('product/show.html.twig', [
             'product' => $product,
+            'review_form' => $form->createView(),
         ]);
     }
 }

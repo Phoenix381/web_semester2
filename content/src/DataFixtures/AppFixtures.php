81614bc82b0d5c2a766b1392d95defd141aec81a
phoenix381 <phoenix0381@gmail.com>
2025-05-02 08:19:34 +0300

    cleaned

--- content/src/DataFixtures/AppFixtures.php
+++ content/src/DataFixtures/AppFixtures.php
@@ -5,12 +5,17 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+use App\Entity\User;
+use App\Entity\Product;
+use App\Entity\Review;
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new User();
+        $admin->setName('admin');
+        $admin->setEmail('admin@localhost');
 
         $manager->flush();
     }
